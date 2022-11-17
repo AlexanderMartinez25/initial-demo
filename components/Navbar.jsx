5c985d841c3ac3b2eab8e6217a45e7d5d7483fab
Alexander Martinez <alexander.martinez@lapatota.cl>
2022-11-17 19:53:41 -0300

    Custom ActiveLink

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,12 @@
-import Link from "next/link";
+import { ActiveLink } from "./ActiveLink";
 import style from "./Navbar.module.css";
+
 export const Navbar = () => {
   return (
-    <nav>
-      <Link className={style["menu-container"]} href="/">
-        Home
-      </Link>
-      <Link href="/about">About</Link>
-      <Link href="/contact">Contact</Link>
+    <nav className={style["menu-container"]}>
+      <ActiveLink text="Home" href="/" />
+      <ActiveLink text="About" href="/about" />
+      <ActiveLink text="Contact" href="/contact" />
     </nav>
   );
 };
